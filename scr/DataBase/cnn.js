4dd896cb3003e10bdb77e9a530b4380edcd652c8
kdrodriguez <kevin.rzz18@gmail.com>
2019-11-06 14:04:24 -0500

    Variables de entorno locales para la DB

--- scr/DataBase/cnn.js
+++ scr/DataBase/cnn.js
@@ -1,3 +1,6 @@
+var env = require('node-env-file'); // .env file
+env(__dirname+"/.env");
+
 //-------------------------------------------------------------------------------------
 // 1. Información de base de datos para registros de API
 //-------------------------------------------------------------------------------------
@@ -6,15 +9,11 @@ const pgp = pgPromise({}); // Empty object means no additional config required
 const config = {
     /*host: 'localhost', //process.env.POSTGRES_HOST,
     port: '5432', //process.env.POSTGRES_PORT,
-    database: 'grql_mendeley_bitacora', //process.env.POSTGRES_DATABASE,
+    database: 'your_db_name', //process.env.POSTGRES_DATABASE,
     user: 'postgres', //process.env.POSTGRES_USER,
-    password: 'admin12345' //process.env.POSTGRES_PASSWORD */
-    host: 'ec2-174-129-253-68.compute-1.amazonaws.com',
-    port: '5432', //process.env.POSTGRES_PORT,
-    database: 'denju6spl4b8r3',
-    user: 'roafuftzlbodfw',
-    password: '4de5d39a241ccd2c452307e934530a509fb916c0745103095f24d3f1b13efe6c',
-    ssl: true, // Permite acceso a la bdd remotamente (app localhost)
+    password: 'your_pass' //process.env.POSTGRES_PASSWORD */
+   connectionString: process.env.DATABASE_URL,
+   ssl: true,
 };
 
 const db = pgp(config);
@@ -24,18 +23,5 @@ exports.db = db;
 // 2. Recuperación de información de base de datos para estadísticas
 //-------------------------------------------------------------------------------------
 const { Pool } = require('pg')
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-})
+const pool = new Pool(config)
 exports.pool = pool;
-
-/*const pool = new Pool({
-  user: 'roafuftzlbodfw',
-  host: 'ec2-174-129-253-68.compute-1.amazonaws.com',
-  database: 'denju6spl4b8r3',
-  password: '4de5d39a241ccd2c452307e934530a509fb916c0745103095f24d3f1b13efe6c',
-  port: 5432,
-  ssl : true, // Permite acceso a la bdd remotamente (app localhost)
-})*/
-
